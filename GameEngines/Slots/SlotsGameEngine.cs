ccd505ed547dc41cec8fb055d80935fd0c67c842
StevenCurbison <100568994+StevenCurbison@users.noreply.github.com>
2023-07-20 21:58:58 +0100

    Validate initial deposit

--- GameEngines/Slots/SlotsGameEngine.cs
+++ GameEngines/Slots/SlotsGameEngine.cs
@@ -4,21 +4,48 @@ namespace GameEngines.Slots
 {
     public class SlotsGameEngine : ISlotsGameEngine
     {
+        private const string AbandonGameKey = "q";
         private decimal _deposit;
 
         public void StartGame()
         {
-            Console.WriteLine("Welcome, Please specify how much you would like to deposit?");
-            var deposit = Console.ReadLine();
+            var deposit = RequestAmount();
             if (IsValidDeposit(deposit))
             {
-
+                PlaceStake(0);
+                return;
             }
+
+            StartGame();
+        }
+
+        private string? RequestAmount()
+        {
+            Console.WriteLine("Welcome, Please specify how much you would like to deposit, or press Q to abandon the game?");
+            var response = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(response))
+                return RequestAmount();
+
+            if (response.ToLower() == AbandonGameKey)
+                CloseApplication();
+
+            return response;
+        }
+
+        private void CloseApplication()
+        {
+            Console.WriteLine("Thank you for playing, we hope to see you soon. Press any key to close");
+            Console.ReadLine();
+            Environment.Exit(0);
         }
 
         private bool IsValidDeposit(string depositEntered)
         {
-            return false;
+            if (!decimal.TryParse(depositEntered, out decimal validDeposit)) 
+                return false;
+
+            _deposit = validDeposit;
+            return true;
         }
 
         public void PlaceStake(decimal initialStake)
